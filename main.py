b0c6c35a8257d4d13d4a67f94cf07c34e5ff6cd3
ihou-david-wateba <ihoudavid.wateba@edu.esiee.fr>
2024-10-25 17:25:20 +0000

    voici mon code qui permet de vérifier si un nombre est premier

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+"""Module pour vérifier si un nombre est premier."""
 from math import sqrt
 
 #### Fonction secondaire
 
 
-def isprime(p):
-
+def isprime(p:int)->bool:
+    """Vérifie si un nombre entier est premier."""
     # votre code ici
-
-    pass
+    rap=sqrt(p)
+    if p<2:
+        return False
+    if p==2:
+        return True
+    # Pour cette boucle nous pouvons l'écrire de 2 manières
+    # for i in range(2, int(rap)+1): ou for i in range(2, p-1): car on vérifie si tous les
+    # nombres compris entre 2(inclus) et p-1(inclus)
+    for i in range(2, int(rap) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Fonction principale pour tester la fonction isprime."""
     # vos appels à la fonction secondaire ici
+        #Tests de la fonction isprime
+    print(isprime(2))#True
+    print(isprime(3))#True
+    print(isprime(4))#False
+    print(isprime(5))#True
+    print(isprime(10))#False
+    print(isprime(17))#True
+    print(isprime(20))#False
 
     for n in range(100):
         if isprime(n):
